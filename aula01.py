4d8e1565d57bbe78d54c7583f180fa4049fb0572
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-10-18 16:51:55 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,29 @@
+# # Aula 01
+# # celsius = float(input('Digite a temperatura em Celsius: '))
+# # print('\nA temperatura em Fahrenheit é de ', 1.8 * celsius + 32)
+#
+# Exercício 01
+# n = int(input('Digite um número: '))
+# print(f'\nO quadrado de {n} é {n**2}.')
+#
+# Exercício 02
+# c1 = input('Digite o primeiro caracter: ')
+# c2 = input('Digite o segundo caracter: ')
+# print(f'\nO usuário digitou "{c1}" e "{c2}".')
+#
+# Exercício 03
+# n = int(input('Digite um número: '))
+# print(f'\nAntecessor de {n}: {n-1}')
+# print(f'Sucessor de {n}: {n+1}')
+#
+# Exercício 04
+# b = float(input('Digite a base do retângulo: '))
+# h = float(input('Digite a altura do retângulo: '))
+# print(f'\nPerímetro do retângulo: {b*2 + h*2}')
+# print(f'Área do retângulo: {b*h}')
+#
+# Exercício 05
+v = float(input('Digite o valor: '))
+tx = float(input('Digite a taxa: '))
+tp = float(input('Digite o tempo: '))
+print(f'\nO valor da prestação é {v + v * (tx / 100) * tp}')
